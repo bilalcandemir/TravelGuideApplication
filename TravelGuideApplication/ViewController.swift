8eeea697849f5dbfcd67f5ac364e476e88fb08e3
Bilal Candemir <candemirbilal@yahoo.com>
2022-09-27 16:39:04 +0300

    Initial Commit

---
+++ TravelGuideApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelGuideApplication
+//
+//  Created by Bilal Candemir on 9/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
